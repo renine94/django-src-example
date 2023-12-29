52c118965bc92ac5254359263497d95410db3039
JaeguKang <renine94.dev@gmail.com>
2023-12-29 13:03:37 +0900

    django 초기 세팅

--- src/wsgi.py
+++ src/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+# gunicorn src.wsgi:application -b 0.0.0.0:8000 -w 4 -e DJANGO_SETTINGS_MODULE=src.core.settings.{env}
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.settings')
 
 application = get_wsgi_application()
